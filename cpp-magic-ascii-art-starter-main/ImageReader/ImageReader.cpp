658c69d02c2290ce4150940319f2d44f04516d2e
Bodis Patrik <bodis.uk@gmail.com>
2025-01-13 13:51:06 +0100

    red bmp implemented

---
+++ cpp-magic-ascii-art-starter-main/ImageReader/ImageReader.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Patrik on 13/01/2025.
+//
+
+#include "ImageReader.h"
--- cpp-magic-ascii-art-starter-main/main.cpp
+++ cpp-magic-ascii-art-starter-main/main.cpp
@@ -1,9 +1,34 @@
 //
 // Created by Patrik on 13/01/2025.
 //
-#include "EasyBMP_1.06/EasyBMP.h"
+#include <filesystem>
+#include <array>
+
 #include <iostream>
+#include "EasyBMP_1.06/EasyBMP.h"
+#include "ImageReader/ImageReader.h"
+
 using namespace std;
-int main(void){
- cout<<"Hello World!"<<endl;
+int main(int argc, char **argv) {
+
+
+    cout<<"Hello World!"<<endl;
+    int width;
+    int height;
+  unsigned char* array = ImageReader::readBmp("../pics/test2.bmp", width, height);
+  for (int i = 0; i < height; i++) {
+    if (i>0) {
+        break;
+    }
+      for (int j = 0; j < width; j++) {
+          for (int k = 0; k < 3; k++) {
+              std::cout << +array[3*width*i+3*j+k]<< " ";
+          }
+          cout << "," ;
+
+      }
+      cout << endl;
+  }
+
+
 }
