353257296160071d724a7c792de41a1036b7b6ed
judit-g <108220413+judit-g@users.noreply.github.com>
2025-01-14 15:49:00 +0100

    PngImageReader

--- cpp-magic-ascii-art-starter-main/src/ImageReader/PngImageReader/PngImageReader.cpp
+++ cpp-magic-ascii-art-starter-main/src/ImageReader/PngImageReader/PngImageReader.cpp
@@ -3,8 +3,21 @@
 //
 
 #include "PngImageReader.h"
-#include "lodepng.h"
+#include "../../external/lodepng/lodepng.h"
+#include <iostream>
 
 PixelArray PngImageReader::readImage(const std::string &filePath) {
-    return *new PixelArray{0, 0, 0};
+    std::vector<unsigned char> image;
+    unsigned int width, height;
+    unsigned error = lodepng::decode(image, width, height, filePath);
+
+    std::cout << image.size() << " " << width << " " << height << std::endl;
+
+    PixelArray pixelArray = PixelArray{static_cast<int>(height), static_cast<int>(width), 3};
+
+    for (int i = 0; i < height; i++) {
+        std::cout << +current << " ";
+    }
+
+    return pixelArray;
 }
--- cpp-magic-ascii-art-starter-main/src/main.cpp
+++ cpp-magic-ascii-art-starter-main/src/main.cpp
@@ -8,31 +8,49 @@
 #include "Grayscale.h"
 #include "PixelArray.h"
 #include "./ImageReader/BmpImageReader/BmpImageReader.h"
+#include "./ImageReader/PngImageReader/PngImageReader.h"
 
 using namespace std;
 
 
 int main(int argc, char **argv) {
     cout << "Hello World!" << endl;
-
     BmpImageReader bmpImageReader;
+    PngImageReader pngImageReader;
 
     Grayscale gs{"@#8&o:*. "};
-    PixelArray array = bmpImageReader.readImage("../pics/test2.bmp");
-    PixelArray grayArray = gs.convert(array);
+    PixelArray bmpArray = bmpImageReader.readImage("../pics/test2.bmp");
+    PixelArray pngArray = pngImageReader.readImage("../pics/test2.png");
 
-    ofstream outFile("asd.txt");
-    if (!outFile.is_open()) {
+    PixelArray grayBmpArray = gs.convert(bmpArray);
+    PixelArray grayPngArray = gs.convert(pngArray);
+
+    ofstream outFileBmp("bmp.txt");
+    if (!outFileBmp.is_open()) {
         throw runtime_error("Error opening file");
     }
-    for (int r = 0; r < grayArray.height; r++) {
+    for (int r = 0; r < grayBmpArray.height; r++) {
         // if (r>12) break;
-        for (int c = 0; c < grayArray.width; c++) {
+        for (int c = 0; c < grayBmpArray.width; c++) {
             // if (c>12) break;
-            outFile << grayArray.getCell(r, c, 0);
+            outFileBmp << grayBmpArray.getCell(r, c, 0);
         }
-        outFile << endl;
+        outFileBmp << endl;
     }
-    outFile.close();
+    outFileBmp.close();
+
+    ofstream outFilePng("png.txt");
+    if (!outFilePng.is_open()) {
+        throw runtime_error("Error opening file");
+    }
+    for (int r = 0; r < grayPngArray.height; r++) {
+        // if (r>12) break;
+        for (int c = 0; c < grayPngArray.width; c++) {
+            // if (c>12) break;
+            outFilePng << grayPngArray.getCell(r, c, 0);
+        }
+        outFilePng << endl;
+    }
+    outFilePng.close();
 
 }
