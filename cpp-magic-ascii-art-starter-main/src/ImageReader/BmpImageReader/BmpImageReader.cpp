f72cdebe63c46b768043310173ff55902c037e80
judit-g <108220413+judit-g@users.noreply.github.com>
2025-01-15 20:23:59 +0100

    PngImageReader added

--- cpp-magic-ascii-art-starter-main/src/ImageReader/BmpImageReader/BmpImageReader.cpp
+++ cpp-magic-ascii-art-starter-main/src/ImageReader/BmpImageReader/BmpImageReader.cpp
@@ -12,9 +12,6 @@ PixelArray BmpImageReader::readImage(const std::string &filePath) {
 
     for (int r = 0; r < array.height; r++) {
         for (int c = 0; c < array.width; c++) {
-            if (c==1) {
-                std::cout << "row: " << r << " col: " << c  << " pixel: " << +bmp.GetPixel(c, r).Red << " " << +bmp.GetPixel(c, r).Blue << std::endl;
-            }
             array.setCell(r, c, 0, bmp.GetPixel(c, r).Red);
             array.setCell(r, c, 1, bmp.GetPixel(c, r).Green);
             array.setCell(r, c, 2, bmp.GetPixel(c, r).Blue);
--- cpp-magic-ascii-art-starter-main/src/ImageReader/PngImageReader/PngImageReader.cpp
+++ cpp-magic-ascii-art-starter-main/src/ImageReader/PngImageReader/PngImageReader.cpp
@@ -8,17 +8,21 @@
 
 PixelArray PngImageReader::readImage(const std::string &filePath) {
     std::vector<unsigned char> image;
+
     unsigned int width, height;
     unsigned error = lodepng::decode(image, width, height, filePath);
 
-    std::cout << image.size() << " " << width << " " << height << std::endl;
-
     PixelArray pixelArray = PixelArray{static_cast<int>(height), static_cast<int>(width), 3};
 
-    for (int row = 0; row < height; row++) {
-        for (int col = 0; col < width; col++) {
-
+    int channelCount = 0;
+    for (int row = 0; row < pixelArray.height; row++) {
+        for (int col = 0; col < pixelArray.width; col++) {
+            pixelArray.setCell(row, col, 0, image[channelCount]);
+            pixelArray.setCell(row, col, 1, image[channelCount + 1]);
+            pixelArray.setCell(row, col, 2, image[channelCount + 2]);
+            channelCount += 4;
         }
     }
+
     return pixelArray;
 }
